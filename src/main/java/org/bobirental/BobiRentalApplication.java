5928388a26c70232bca70bd91086232b72c3dbde
blrhn <mm9854815@gmail.com>
2025-11-08 17:00:47 +0100

    chore: init

---
+++ src/main/java/org/bobirental/BobiRentalApplication.java
@@ -0,0 +1,13 @@
+package org.bobirental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobiRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobiRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bobirental/BobiRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bobirental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobiRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
